eea2e2bb7482b277c4f2add24fc5f8c420a03bfc
MarkGravestock <mark.gravestock@gmail.com>
2023-12-05 20:39:38 +0000

    Day 5 Part 1 solving real values.

---
+++ src/main/kotlin/day05/Almanac.kt
@@ -0,0 +1,79 @@
+package day05
+
+class Almanac(private val fileInput: List<String>) {
+    private fun extractSeedParts(line: String) = ("seeds: ((?:\\d+\\s*)+)".toRegex()).find(line)
+    fun seeds(): List<Long> {
+        val seeds = extractSeedParts(fileInput.first())
+        return seeds!!.groups[1]?.value?.trim()?.split("\\s+".toRegex())!!.map { it.toLong() }
+    }
+
+    fun maps(): Map<String, CategoryMap> {
+        val categoryMaps = mutableMapOf<String, CategoryMap>()
+        var currentMap = CategoryMap.empty()
+
+        for (line in 2..<fileInput.size) {
+            val lineContent = fileInput[line]
+
+            if (lineContent.isBlank()) continue
+
+            if (lineContent.contains(" map:")) {
+                currentMap = CategoryMap(lineContent)
+                categoryMaps[lineContent] = currentMap
+            } else {
+                val (destinationRangeStart, sourceRangeStart, rangeLength) = lineContent.split(" ").map { it.toLong() }
+                currentMap.addMapping(Mapping(destinationRangeStart, sourceRangeStart, rangeLength))
+            }
+        }
+
+        return categoryMaps
+    }
+
+    fun mapOriginalSourceOf(source: Long): Long {
+        var nextValue = source
+
+        for (map in maps()) {
+            nextValue = map.value.mapSourceOf(nextValue)
+        }
+
+        return nextValue
+    }
+
+    fun findLowestFinalDestination(): Long {
+        return seeds().minOf { mapOriginalSourceOf(it) }
+    }
+
+}
+
+class Mapping(val destinationRangeStart: Long, val sourceRangeStart: Long, val rangeLength: Long) {
+
+    private val range = LongRange(start = sourceRangeStart, endInclusive = sourceRangeStart + rangeLength - 1)
+    fun canMap(value: Long): Boolean {
+        return range.contains(value)
+    }
+
+    fun mappedValue(value: Long): Long {
+        return if (canMap(value)) (value - sourceRangeStart) + destinationRangeStart else value
+    }
+
+}
+
+class CategoryMap(val line: String) {
+
+    private val mappings: MutableList<Mapping> = mutableListOf()
+    fun addMapping(mapping: Mapping) {
+        mappings.add(mapping)
+    }
+
+    fun size(): Int = mappings.size
+    fun mapSourceOf(value: Long): Long {
+        val mapping = mappings.singleOrNull { it.canMap(value) }
+        return mapping?.mappedValue(value) ?: value
+    }
+
+    companion object {
+        fun empty(): CategoryMap {
+            return CategoryMap("error")
+        }
+    }
+
+}
--- src/test/kotlin/Day05Part1Test.kt
+++ src/test/kotlin/Day05Part1Test.kt
@@ -1,3 +1,5 @@
+import day05.Almanac
+import day05.Mapping
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
@@ -28,9 +30,9 @@ class Day05Part1Test : FunSpec({
             val first = sut.maps().values.first()
 
             forAll(
-                row(97, 99),
-                row(99, 51),
-                row(0, 0),
+                row(97L, 99),
+                row(99L, 51),
+                row(0L, 0),
             ) { source, destination ->
                 first.mapSourceOf(source) shouldBe destination
             }
@@ -38,10 +40,10 @@ class Day05Part1Test : FunSpec({
 
         test("It can map from original source to final destination") {
             forAll(
-                row(79, 82),
-                row(14, 43),
-                row(13, 35),
-                row(55, 86),
+                row(79L, 82),
+                row(14L, 43),
+                row(13L, 35),
+                row(55L, 86),
             ) { source, destination ->
                 sut.mapOriginalSourceOf(source) shouldBe destination
             }
@@ -57,10 +59,10 @@ class Day05Part1Test : FunSpec({
 
         test("It can map") {
             forAll(
-                row(97, false),
-                row(98, true),
-                row(99, true),
-                row(100, false)
+                row(97L, false),
+                row(98L, true),
+                row(99L, true),
+                row(100L, false)
             ) { source, expectedCanMap ->
                 sut.canMap(source) shouldBe expectedCanMap
             }
@@ -68,93 +70,25 @@ class Day05Part1Test : FunSpec({
 
         test("It maps value") {
             forAll(
-                row(97, 97),
-                row(98, 50),
-                row(99, 51),
-                row(100, 100),
+                row(97L, 97),
+                row(98L, 50),
+                row(99L, 51),
+                row(100L, 100),
             ) { source, destination ->
                 sut.mappedValue(source) shouldBe destination
             }
         }
     }
 
+        context("Real File Seeds logic for Almanac") {
+
+        val fileInput = readInputForDay(day)
+        val sut = Almanac(fileInput)
+
+        test("It can find lowest final destination") {
+            sut.findLowestFinalDestination() shouldBe 240320250
+        }
+    }
+
 })
-
-class Almanac(private val fileInput: List<String>) {
-    private fun extractSeedParts(line: String) = ("seeds: ((?:\\d+\\s*)+)".toRegex()).find(line)
-    fun seeds(): List<Int> {
-        val seeds = extractSeedParts(fileInput.first())
-        return seeds!!.groups[1]?.value?.trim()?.split("\\s+".toRegex())!!.map { it.toInt() }
-    }
-
-    fun maps(): Map<String, CategoryMap> {
-        val categoryMaps = mutableMapOf<String, CategoryMap>()
-        var currentMap = CategoryMap.empty()
-
-        for (line in 2..<fileInput.size) {
-            val lineContent = fileInput[line]
-
-            if (lineContent.isBlank()) continue
-
-            if (lineContent.contains(" map:")) {
-                currentMap = CategoryMap(lineContent)
-                categoryMaps[lineContent] = currentMap
-            } else {
-                val (destinationRangeStart, sourceRangeStart, rangeLength) = lineContent.split(" ").map { it.toInt() }
-                currentMap.addMapping(Mapping(destinationRangeStart, sourceRangeStart, rangeLength))
-            }
-        }
-
-        return categoryMaps
-    }
-
-    fun mapOriginalSourceOf(source: Int): Int {
-        var nextValue = source
-
-        for (map in maps()) {
-            nextValue = map.value.mapSourceOf(nextValue)
-        }
-
-        return nextValue
-    }
-
-    fun findLowestFinalDestination(): Int {
-        return seeds().minOf { mapOriginalSourceOf(it) }
-    }
-
-}
-
-class Mapping(val destinationRangeStart: Int, val sourceRangeStart: Int, val rangeLength: Int) {
-
-    private val range = IntRange(start = sourceRangeStart, endInclusive = sourceRangeStart + rangeLength - 1)
-    fun canMap(value: Int): Boolean {
-        return range.contains(value)
-    }
-
-    fun mappedValue(value: Int): Int {
-        return if (canMap(value)) (value - sourceRangeStart) + destinationRangeStart else value
-    }
-
-}
-
-class CategoryMap(val line: String) {
-
-    private val mappings: MutableList<Mapping> = mutableListOf()
-    fun addMapping(mapping: Mapping) {
-        mappings.add(mapping)
-    }
-
-    fun size(): Int = mappings.size
-    fun mapSourceOf(value: Int): Int {
-        val mapping = mappings.singleOrNull { it.canMap(value) }
-        return mapping?.mappedValue(value) ?: value
-    }
-
-    companion object {
-        fun empty(): CategoryMap {
-            return CategoryMap("error")
-        }
-    }
-
-}
 
